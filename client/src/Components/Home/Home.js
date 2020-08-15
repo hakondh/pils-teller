df8bbfa9a0bd6ccaed85877e4684f58a9c98a32f
hakondh <hakonhd@live.no>
2020-08-15 12:58:04 +0200

    Added weekly drunkards to home

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -14,12 +14,11 @@ function Home(props) {
         var count = 0;
         res.data.forEach((b) => {
           count += b.amount;
-          console.log(b.amount);
         });
         setBeers(count);
       })
       .catch((err) => console.log(err));
-  });
+  }, []);
 
   return (
     <section id="main">
@@ -27,7 +26,7 @@ function Home(props) {
         <h1 className="header">{beers} pils</h1>
         <p className="text">har blitt drukket siden 13/8/2020.</p>
         <br />
-        {/* <WeeklyDrunkard></WeeklyDrunkard> */}
+        <WeeklyDrunkard></WeeklyDrunkard>
       </div>
     </section>
   );
--- client/src/Components/WeeklyDrunkard/WeeklyDrunkard.js
+++ client/src/Components/WeeklyDrunkard/WeeklyDrunkard.js
@@ -2,18 +2,32 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 function WeeklyDrunkard(props) {
-  const [drunkard, setDrunkard] = useState(undefined);
+  const [drunkards, setDrunkards] = useState([]);
+  const [titleText, setTitleText] = useState("Ukens fyllik:");
+
   useEffect(() => {
-    axios.get("/beers/weekly-drunkard").then((res) => {
-      console.log(res);
-      setDrunkard(res.data[0]);
-    });
-  });
+    axios
+      .get("/beers/weekly-drunkard")
+      .then((res) => {
+        setDrunkards(res.data);
+        if (res.data.length > 1) setTitleText("Ukens fylliker:");
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <div>
-      <h2>Ukens fyllik(er):</h2>
-      {drunkard && <p>{drunkard}</p>}
+      <h2>{titleText}</h2>
+      <p>
+        {drunkards.map((drunkard, i) => (
+          <span key={drunkard.NAME}>
+            {drunkard.NAME}
+            {i < drunkards.length - 1
+              ? [i == drunkards.length - 2 ? " og " : ", "]
+              : ", med " + drunkard.count + " pils."}
+          </span>
+        ))}
+      </p>
     </div>
   );
 }
