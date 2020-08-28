8307a3d47e8c34f586c41412ab1bd06f18d5b23a
hakondh <hakonhd@live.no>
2020-08-28 10:32:13 +0200

    Fixes #2

--- client/src/Components/Profile/Profile.js
+++ client/src/Components/Profile/Profile.js
@@ -3,14 +3,15 @@ import axios from "axios";
 
 function Profile(props) {
   const user = JSON.parse(localStorage.getItem("user"));
-  const [consumedBeers, setConsumedBeers] = useState(null);
-  const [averageBeers, setAverageBeers] = useState(null);
+  const [consumedBeers, setConsumedBeers] = useState(0);
+  const [averageBeers, setAverageBeers] = useState(0);
 
   useEffect(() => {
     axios
       .get("/users/" + user.id + "/beers")
       .then((res) => {
-        setConsumedBeers(res.data[0].sum);
+        const fetchedSum = res.data[0].sum;
+        if (fetchedSum) setConsumedBeers(fetchedSum);
       })
       .catch((err) => console.log(err));
   }, []);
@@ -20,6 +21,7 @@ function Profile(props) {
   }, [consumedBeers]);
 
   const getAverage = () => {
+    if (consumedBeers === 0) return setAverageBeers(0); // Avoid all this logic if the amount is 0
     const regDate = Date.parse(
       JSON.parse(localStorage.getItem("user")).reg_date
     );
@@ -45,7 +47,7 @@ function Profile(props) {
       <p>
         Antall pils konsumert: <span>{consumedBeers}</span>
       </p>
-      {averageBeers && averageBeers !== Infinity && (
+      {averageBeers !== Infinity && (
         <p>
           Det er i gjennomsnitt <span>{averageBeers}</span> pils per dag.
         </p>
